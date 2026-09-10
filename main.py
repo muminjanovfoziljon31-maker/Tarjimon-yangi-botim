dbb37b5b860666b15059690dbebab081eb8991f6
muminjanovfoziljon31-maker <muminjanovfoziljon31@gmail.com>
2026-09-10 09:10:08 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1776,14 +1776,15 @@ def document_handler(message):
 
 
 # ============================================================
-# XATOLIK HANDLER
+# BOT ISHLASHI
 # ============================================================
 
-@bot.middleware_handler(
-    update_types=["message"]
-)
-def middleware_handler(bot_instance, message):
-    pass
+if __name__ == "__main__":
+    bot.infinity_polling(
+        skip_pending=True,
+        timeout=60,
+        long_polling_timeout=60
+    )
 
 
 # ============================================================
